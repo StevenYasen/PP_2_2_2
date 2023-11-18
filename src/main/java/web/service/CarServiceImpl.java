bce533a8be3b1a893cffe3b9b9d6874340145dc8
YasenitskyStepan <yastepan@mail.ru>
2023-11-18 16:03:23 +0300

    no code or import in comments, constructior @autowiring-final variables, all logic in service layer, no copy of array(was really bad solution- im sorry) but adding to list and clearing it

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -2,13 +2,12 @@ package web.service;
 
 import org.springframework.stereotype.Service;
 import web.model.Car;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CarServiceImpl implements CarService {
+    List<Car> carlist = new ArrayList<>();
     private Car[] cars = {
             new Car(1999, "Honda", 777),
             new Car(2008, "Hyindai", 1745),
@@ -17,10 +16,13 @@ public class CarServiceImpl implements CarService {
             new Car(2000, "Zapor", 111)
     };
 
-
     @Override
-    public List<Car> createCars(int n) {
-        return Arrays.asList(Arrays.copyOf(cars, n));
+    public List<Car> showCarsByNumber(Integer count) {
+        carlist.clear();
+        count = (count == null || count > 5) ? 5 : count < 0 ? 0 : count;
+        for (int i = 0; i < count; i++) {
+            carlist.add(cars[i]);
+        }
+        return carlist;
     }
-
 }
